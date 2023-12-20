63fb9e7e8819b11202f2ca22b7557780c870ffa6
nanashi-1 <bagaresalken@gmail.com>
2023-12-20 08:35:14 +0000

    add extra plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -201,5 +201,32 @@ require("lazy").setup({
 				return vim.ui.input(...)
 			end
 		end,
+	},
+	{
+
+		"rcarriga/nvim-notify",
+		init = function()
+			vim.notify = require("notify")
+		end
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+			{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+		},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl"
+	},
+	{
+		'echasnovski/mini.indentscope',
+		version = '*'
 	}
 })
