c74b91b3c3cb6f58fd337dd0e9f45d31867e4745
nanashi-1 <bagaresalken@gmail.com>
2023-12-20 08:35:25 +0000

    fix rust-tools config

--- lua/plugins/configs/rust-tools.lua
+++ lua/plugins/configs/rust-tools.lua
@@ -1,4 +1,6 @@
-require("rust-tools").setup({
+local rt = require("rust-tools")
+
+rt.setup({
 	server = {
 		on_attach = function(_, bufnr)
 			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
