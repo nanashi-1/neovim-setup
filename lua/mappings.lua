321488ccb618bde3c5b00a682a264c6ddcc10a32
nanashi-1 <bagaresalken@gmail.com>
2023-12-20 08:35:00 +0000

    simplify mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,4 @@
-local function map(mode, lhs, rhs, opts)
-	local options = { noremap = true, silent = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local map = vim.keymap.set
 
 map("n", "<C-h>", ":wincmd h<CR>")
 map("n", "<C-j>", ":wincmd j<CR>")
@@ -15,11 +9,11 @@ map("n", "<C-w>", ":q<CR>")
 map("n", "<tab>", ":bnext<CR>")
 map("n", "<leader>x", ":bdelete<CR>:bnext<CR>")
 
-map("n", "<C-s>", ":w<CR>")
+map({ "n", "i", "v" }, "<C-s>", ":w<CR>")
 map("n", "<leader>pf", ":Telescope find_files<CR>")
 map("n", "<leader>pb", ":Telescope buffers<CR>")
 map("n", "<leader>e", ":NvimTreeToggle<CR>")
 map("n", "<leader>pg", ":LazyGit<CR>")
-vim.keymap.set({ "n" }, "<leader>fm", function()
+map({ "n" }, "<leader>fm", function()
 	vim.lsp.buf.format({ async = true })
 end)
